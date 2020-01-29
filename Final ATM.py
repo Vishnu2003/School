34dbdddaaf9509853c1070f148fee2cf4af12385
Vishnu2003 <60405417+Vishnu2003@users.noreply.github.com>
2020-01-30 00:57:27 +0530

    Update Final ATM.py

--- Final ATM.py
+++ Final ATM.py
@@ -1,14 +1,15 @@
-acc_no={12345:1234,
-        23451:2341,
-        34512:3412,
-        45123:4123,
-        51234:4321}
+acc_no = {12345: 1234,
+          23451: 2341,
+          34512: 3412,
+          45123: 4123,
+          51234: 4321}
 
-acc_bal={12345:364734,
-        23451:5637564,
-        34512:552753,
-        45123:7275,
-        51234:58245}
+
+acc_bal = {12345: 364734,
+           23451: 5637564,
+           34512: 552753,
+           45123: 7275,
+           51234: 58245}
 
 
 def run():
@@ -22,51 +23,72 @@ def run():
             continue
 
     while True:
-        pin = int(input('Enter your pincode: '))
+        pin = int(input('Enter your pin code: '))
         if acc_no[user] == pin:
             break
         else:
-            print('Pincode is wrong try again: ')
+            print('Pin code is wrong try again: ')
 
+    print('Select an action')
+    print('Select 1 to check balance')
+    print('Select 2 withdraw')
+    print('Select 3 to deposit')
+    print('Select 4 to transfer')
 
-    print('select an action ')
-    print('select 1 to check balence')
-    print('select 2 withdraw')
-    print('select 3 to deposit')
-    print('select 4 to transfer')
+    while True:
+        usr_actn = int(input('Enter the action: '))
+        if usr_actn == 1:
+            print('The amount in your account is: ', acc_bal[user])
+            print('THANK YOU!!!...')
+            break
+        elif usr_actn == 2:
+            print('The amount in your account is: ', acc_bal[user])
+            withdraw = int(input('Enter the amount you want to withdraw: '))
+            while True:
+                acc_bal[user] = acc_bal[user] - withdraw
+                if acc_bal[user] < 0:
+                    print('Balance is insufficient')
+                    acc_bal[user] = acc_bal[user] + withdraw
+                    break
+                else:
+                    print('Current balance: ', acc_bal[user])
+                    print('THANK YOU!!!...')
+                    break
+            break
+        elif usr_actn == 3:
+            print('The amount in your account is: ', acc_bal[user])
+            deposit = int(input('Enter the amount you want to deposit: '))
+            acc_bal[user] = acc_bal[user] + deposit
+            print('Current balance: ', acc_bal[user])
+            print('THANK YOU!!!...')
+            break
+        elif usr_actn == 4:
+            print('The amount in your account is: ', acc_bal[user])
+            while True:
+                t_acnt = int(input('Enter the account you want to transfer: '))
+                if t_acnt in acc_no.keys() and t_acnt != user:
+                    print('The amount in the transfer account: ', acc_bal[t_acnt])
+                    t_amt = int(input('Enter the amount you want to transfer: '))
+                    while True:
+                        acc_bal[user] = acc_bal[user] - t_amt
+                        if acc_bal[user] < 0:
+                            print('Balance is insufficient')
+                            acc_bal[user] = acc_bal[user] + t_amt
+                            break
+                        else:
+                            acc_bal[t_acnt] = acc_bal[t_acnt] + t_amt
+                            print('Current balance: ', acc_bal[user])
+                            print('The amount in the transferees account: ', acc_bal[t_acnt])
+                            print('THANK YOU!!!...')
+                            break
+                    break
+                else:
+                    print('Enter transfer account number correctly')
+                    continue
+            break
+        else:
+            print('Enter the option given in the menu')
 
-    usr_actn=int(input('Enter the action: '))
-    if usr_actn == 1:
-        print('The ammount in your account is: ', acc_bal[user])
-        print('THANK YOU!!!...')
-    elif usr_actn == 2:
-        print('The ammount in your account is: ', acc_bal[user])
-        withdraw=int(input('Enter the ammount you want to withdraw: '))
-        acc_bal[user]=acc_bal[user]-withdraw
-        print('Current balence: ',acc_bal[user])
-        print('THANK YOU!!!...')
-    elif usr_actn == 3:
-        print('The ammount in your account is: ', acc_bal[user])
-        deposit = int(input('Enter the ammount you want to deposit: '))
-        acc_bal[user] = acc_bal[user] + deposit
-        print('Current balence: ', acc_bal[user])
-        print('THANK YOU!!!...')
-    else:
-        print('The ammount in your account is: ', acc_bal[user])
-        while True:
-            t_acnt = int(input('Enter the account you want to transfer: '))
-            if t_acnt in acc_no.keys():
-                print('The ammount in the transfer account: ',acc_bal[t_acnt])
-                t_amnt = int(input('Enter the ammount you want to transfer: '))
-                acc_bal[user] = acc_bal[user]-t_amnt
-                acc_bal[t_acnt] = acc_bal[t_acnt]+t_amnt
-                print('Current balence: ', acc_bal[user])
-                print('The ammont in the transferd account: ',acc_bal[t_acnt])
-                print('THANK YOU!!!....')
-                break
-            else:
-                print('Enter transfer account number correctly')
-                continue
 
 run()
 
@@ -76,10 +98,3 @@ while True:
         run()
     else:
         break
-
-
-
-
-
-
-
