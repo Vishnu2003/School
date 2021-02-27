dcb9da438128d54787d6c779960b5407176ff838
Vishnu2003 <60405417+Vishnu2003@users.noreply.github.com>
2021-02-28 01:22:54 +0530

    Update Project COVID.py

--- Project COVID.py
+++ Project COVID.py
@@ -7,6 +7,7 @@ from time import sleep # To add delay to the code
 import os # To access files in the computer
 from art import * # To add ascii art to the code
 from prettytable import PrettyTable # To print table from an dictionary
+import lxml #To read lxml files
 
 def Global_info():
 
@@ -68,6 +69,8 @@ def Global_info():
             for i in range(len(con_data)):
                 if con_data[i]['CountryCode'] == usr_code:
                     del con_data[i]['Slug']
+                    del con_data[i]['ID']
+                    del con_data[i]['Premium']
                     for key, value in con_data[i].items():
                         pt = PrettyTable()#Creating a table
                         pt.field_names = con_data[i].keys()#Adding field names of the table
@@ -84,12 +87,14 @@ def Global_info():
 
         """Function to Get data by enter an country from global information """
 
-        con = input('Enter the Country which you want the details: ')
+        con = input('Enter the Country which you want the details: ').capitalize()
 
         try:
             for i in range(len(con_data)):
                 if con_data[i]['Country'] == con:
                     del con_data[i]['Slug']
+                    del con_data[i]['ID']
+                    del con_data[i]['Premium']
                     for key, value in con_data[i].items():
                         pt = PrettyTable()#Creating a table
                         pt.field_names = con_data[i].keys()#Adding field names of the table
@@ -193,8 +198,11 @@ def State_info():
 
         try:
             usr_state = input('Enter the state In India: ').title()
-            state_val = states[usr_state]
-            print('\nThere are {} cases in {}\n'.format(state_data[state_val], usr_state))
+            try:
+                print('\nThere are {} cases in {}\n'.format(state_data[usr_state.lower()], usr_state))
+            except:
+                state_val = states[usr_state]
+                print('\nThere are {} cases in {}\n'.format(state_data[state_val], usr_state))
 
         except:
             print('State Does Not Exist or check the spelling ...Try Again...')
@@ -205,70 +213,6 @@ def State_info():
     live_data()
     data_by_state()
     
-def District_info():
-
-    """Function to get District information"""
-
-    def live_data():
-
-        """ To get the live data from https://api.covid19india.org/districts_daily.json """
-
-        global d_data
-        global district_data
-
-        try:
-            #To Check the internet connection
-            print('Trying to connect to network...'); sleep(2)
-            api = 'https://api.covid19india.org/districts_daily.json'
-            req = requests.get(api).text
-            raw_data = bs(req, 'lxml').text
-            text = open("district_data.txt","w")
-            text.write(raw_data)
-            text.close()
-            d_data = eval(open('district_data.txt').read())
-            district_data = d_data['districtsDaily']
-            print('Connection... Successful...Proceeding...'); sleep(2)
-
-        except:
-            #When device is not connected to the internt
-            print('Network Error...Trying with old data...'); sleep(3)
-
-            try:
-                #Trying to read data from the backup
-                d_data = eval(open('district_data.txt').read())
-                district_data = d_data['districtsDaily']
-
-            except:
-                # When the data cannot retrived from the backup
-                print('Cannot retrive the data...Connect to a Network and try again...'); sleep(1)
-                tprint('\nTHANK YOU \n', font='Bold-small'); sleep(3) # Thank you text in ascii art
-                os.remove('district_data.txt')#Deleting the unusable data
-                exit() 
-
-    def data_by_district():
-
-        """Function to Get data by entering state and district from district information """
-
-        try:
-            usr_state = input('Enter the state of the district: ').title()
-            usr_district = input('Enter the district: ').title()
-            district_val = district_data[usr_state][usr_district][-1]
-            del district_val['notes']
-
-            pt = PrettyTable()#Creating a table
-            pt.field_names = district_val.keys()#Adding field names to the table
-            pt.add_row(district_val.values())#Adding values to the table
-            print(pt)
-    
-        except:
-            print('State or District Does Not Exist...Try Again...')
-            data_by_district()
-
-    #Calling nested functions of District_Info()
-
-    live_data()
-    data_by_district()
-    
 def mainloop():
 
     """Function to control the flow of the code"""
@@ -278,13 +222,12 @@ def mainloop():
 
     print('1. Global Information')
     print('2. Indian State Information')
-    print('3. Indian District Information')
     
     while True:
         usr_option = int(input('Enter the option: '))
 
         #To check wether the options are entered correctly
-        if usr_option in (1, 2, 3):
+        if usr_option in (1, 2):
             break
         else:
             print('Select correct option...Try Again...\n')
@@ -299,10 +242,6 @@ def mainloop():
     if usr_option == 2:
         tprint('STATE INFORMATION \n', font='small')# STATE INFORMATION text in ascii art
         State_info()
-    #When user selects option 3
-    if usr_option == 3:
-        tprint('DISTRICT INFORMATION \n', font='small')# DISTRICT INFORMATION text in ascii art
-        District_info()
     
     # To ask the user to run the code again
 
@@ -324,10 +263,3 @@ def mainloop():
 mainloop()
 
 # End of the code
-
-
-
-
-
-
-
